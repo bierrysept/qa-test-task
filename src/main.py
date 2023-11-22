b5679d609b8b354e9fe560235a2fed6e9b0ff12b
Bierrysept <bierrysept@gmail.com>
2023-11-22 20:11:34 +0300

    Storage Site Data

---
+++ src/main.py
@@ -0,0 +1,130 @@
+site_popularity = (
+    {
+        "website": "Google",
+        "populatity": 2800000000,
+        "frontend": "JavaScript, TypeScript",
+        "backend": "C, C++, Go, Java, Python, Node",
+        "datebases": "Bigtable, MariaDB",
+        "notes": "The most used search engine in the world."
+    },
+    {
+        "website": "Facebook",
+        "populatity": 1120000000,
+        "frontend": "JavaScript, Typescript, Flow",
+        "backend": "Hack/HHVM, Python, C++, Java, Erlang, D, Haskell",
+        "datebases": "MariaDB, MySQL, HBase, Cassandra",
+        "notes": "The most visited social networking site."
+    },
+    {
+        "website": "YouTube",
+        "populatity": 1100000000,
+        "frontend": "JavaScript, TypeScript",
+        "backend": "Python, C, C++, Java, Go",
+        "datebases": "Vitess, BigTable, MariaDB",
+        "notes": "The most popular video sharing site."
+    },
+    {
+        "website": "Yahoo",
+        "populatity": 750000000,
+        "frontend": "JavaScript",
+        "backend": "PHP",
+        "datebases": "PostgreSQL, HBase, Cassandra, MongoDB",
+        "notes": ""
+    },
+    {
+        "website": "Etsy",
+        "populatity": 516000000,
+        "frontend": "JavaScript",
+        "backend": "PHP",
+        "datebases": "MySQL, Redis",
+        "notes": "E-commerce website."
+    },
+    {
+        "website": "Amazon",
+        "populatity": 2400000000,
+        "frontend": "JavaScript",
+        "backend": "Java, C++, Perl",
+        "datebases": "DynamoDB, RDS/Aurora, Redshift",
+        "notes": "The most used search engine in the world."
+    },
+    {
+        "website": "Wikipedia",
+        "populatity": 475000000,
+        "frontend": "JavaScript",
+        "backend": "PHP",
+        "datebases": "MariaDB",
+        "notes": "A free online encyclopedia based on MediaWiki, which is programmed in PHP."
+    },
+    {
+        "website": "Fandom",
+        "populatity": 315000000,
+        "frontend": "JavaScript",
+        "backend": "PHP",
+        "datebases": "MySQL",
+        "notes": "Wiki hosting service."
+    },
+    {
+        "website": "X",
+        "populatity": 290000000,
+        "frontend": "JavaScript, ",
+        "backend": "C++, Java, Scala, Ruby (Ruby On Rails)",
+        "datebases": "MySQL",
+        "notes": "Popular social network."
+    },
+    {
+        "website": "Bing",
+        "populatity": 285000000,
+        "frontend": "JavaScript",
+        "backend": "C++, C#",
+        "datebases": "Microsoft SQL Server, Cosmos DB",
+        "notes": "Search engine from Microsoft."
+    },
+    {
+        "website": "eBay",
+        "populatity": 285000000,
+        "frontend": "JavaScript",
+        "backend": "Java, JavaScript, Scala",
+        "datebases": "Oracle Database",
+        "notes": "Online auction house."
+    },
+    {
+        "website": "MSN",
+        "populatity": 280000000,
+        "frontend": "JavaScript",
+        "backend": "C# (ASP.NET)",
+        "datebases": "Microsoft SQL Server",
+        "notes": "An email client, for simple use. Previously known as \"messenger\", not to be confused with Facebook's messaging platform."
+    },
+    {
+        "website": "LinkedIn",
+        "populatity": 260000000,
+        "frontend": "JavaScript",
+        "backend": "Java, JavaScript, Scala",
+        "datebases": "Venice",
+        "notes": "World's largest professional network."
+    },
+    {
+        "website": "Pinterest",
+        "populatity": 250000000,
+        "frontend": "JavaScript",
+        "backend": "Python (Django), Erlang, Elixir",
+        "datebases": "MySQL, Redis",
+        "notes": "Search engine for ideas."
+    },
+    {
+        "website": "WordPress.com",
+        "populatity": 240000000,
+        "frontend": "JavaScript",
+        "backend": "PHP",
+        "datebases": "MariaDB",
+        "notes": "Website manager software."
+    },
+    {
+        "website": "Netflix",
+        "populatity": 223090000,
+        "frontend": "JavaScript",
+        "backend": "Java, Python",
+        "datebases": "NMDB, PostgreSQL",
+        "notes": "The biggest video streaming service in the world."
+    },
+)
